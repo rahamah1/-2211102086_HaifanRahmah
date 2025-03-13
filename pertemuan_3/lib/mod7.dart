9a8d623a4b359dbb7382c6a77d2b84468bf6de88
rahamah1 <rahmah0haifan@gmail.com>
2025-03-13 22:25:08 +0700

    pertemuan_3

---
+++ pertemuan_3/lib/mod7.dart
@@ -0,0 +1,132 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+void main() => runApp(MaterialApp(
+      theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+        color: Colors.amber,
+      )),
+      home: const MyApp(),
+      debugShowCheckedModeBanner: false,
+    ));
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+
+  @override
+  void initState() {
+    super.initState();
+
+    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    final DarwinInitializationSettings initializationSettingsIOS =
+        DarwinInitializationSettings(
+      requestAlertPermission: true,
+      requestBadgePermission: true,
+      requestSoundPermission: true,
+    );
+
+    final InitializationSettings initSettings = InitializationSettings(
+        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+
+    flutterLocalNotificationsPlugin.initialize(
+      initSettings,
+      onDidReceiveNotificationResponse: (NotificationResponse response) {
+        onSelectNotification(response.payload);
+      },
+    );
+  }
+
+  Future<void> onSelectNotification(String? payload) async {
+    if (payload != null && context.mounted) {
+      Navigator.of(context).push(MaterialPageRoute(
+        builder: (_) => NewScreen(payload: payload),
+      ));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: const Text('Flutter Notification Demo'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              width: 250.0,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueAccent,
+                ),
+                onPressed: showNotification,
+                child: const Text(
+                  'Show Notification',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> showNotification() async {
+    const AndroidNotificationDetails androidDetails =
+        AndroidNotificationDetails(
+      'id',
+      'channel',
+      channelDescription: 'description',
+      importance: Importance.max,
+      priority: Priority.high,
+      ticker: 'ticker',
+    );
+
+    const DarwinNotificationDetails iOSDetails = DarwinNotificationDetails();
+    const NotificationDetails platformDetails =
+        NotificationDetails(android: androidDetails, iOS: iOSDetails);
+
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'Flutter Devs',
+      'Flutter Local Notification Demo',
+      platformDetails,
+      payload: 'Welcome to the Local Notification demo',
+    );
+  }
+}
+
+class NewScreen extends StatelessWidget {
+  final String payload;
+  const NewScreen({super.key, required this.payload});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(payload),
+      ),
+      body: const Center(
+        child: Text(
+          'This is the new screen',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
